08ced81d1c8ccdf9633a44937f55275e92ea5948
odaysleft <vincentarasa254@gmail.com>
2025-04-17 12:45:31 +0300

    styling and making the button work, niot yet working

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
 
-let meal_name = document.getElementsByClassName("meal-name")[0];
 let showAllMeals = document.getElementsByClassName("show-meal")[0];
 let meal_photo = document.querySelector("img");
 let meal_intr = document.getElementsByClassName("meal-instructions")[0];
-let search = document.getElementsByClassName("search-food")[0];
-const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search|| ""}`;
- let d;
+let search_meal = document.getElementById("search-food").value;
+let mealValue = document.getElementById("search-food").value;
+let search = document.getElementById("searchBtn").addEventListener('click', 
+     () => {
+          
+          mealValue = search_meal.value;
+     }
+)
+
+console.log(mealValue)
+
+const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealValue}`;
+let d;
 food();
 let meals;
 async function food() {
@@ -14,15 +23,14 @@ async function food() {
           d = await r.json();
 
           meals = d.meals[0]
+          console.log(meals)
           dispayFood()
-          console.log(d.meals.length)
      }
 
 function dispayFood() {
 
           
           for(let i = 0; i <= d.meals.length - 1; i++){
-               console.log(d.meals[i].strMeal);
                 showAllMeals.innerHTML += `
                          <p class="meal-name">${d.meals[i].strMeal}</p>
                          <img src="${d.meals[i].strMealThumb}" class="meal-img">
