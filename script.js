05fb87497077fc61fc6c4c9fcb27fb94f3669821
odaysleft <vincentarasa254@gmail.com>
2025-04-17 12:58:15 +0300

    cleaning code

--- script.js
+++ script.js
@@ -3,33 +3,27 @@
 let showAllMeals = document.getElementsByClassName("show-meal")[0];
 let meal_photo = document.querySelector("img");
 let meal_intr = document.getElementsByClassName("meal-instructions")[0];
-let search_meal = document.getElementById("search-food").value;
-let mealValue = document.getElementById("search-food").value;
+let mealValue = document.getElementById("search-food") || "";
 let search = document.getElementById("searchBtn").addEventListener('click', 
      () => {
-          
-          mealValue = search_meal.value;
+          food();
+          console.log("Meal Value is: " + mealValue.value)
      }
 )
 
-console.log(mealValue)
-
-const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealValue}`;
+let api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealValue || ""}`
 let d;
 food();
 let meals;
 async function food() {
           let r = await fetch(api)
           d = await r.json();
-
-          meals = d.meals[0]
-          console.log(meals)
           dispayFood()
      }
 
 function dispayFood() {
 
-          
+          console.log("There are : " + d.meals.length)
           for(let i = 0; i <= d.meals.length - 1; i++){
                 showAllMeals.innerHTML += `
                          <p class="meal-name">${d.meals[i].strMeal}</p>
