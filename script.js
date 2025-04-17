04cebf7ee1bfac5e888b09e9defef61842a8e128
odaysleft <vincentarasa254@gmail.com>
2025-04-17 11:49:22 +0300

    added css

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 
 
-let meal_name = document.getElementsByClassName("search-food")[0];
-let meal_photo = document.getElementsByClassName("search-food")[0];
-let meal_intr = document.getElementsByClassName("meal-instruction")[0];
+let meal_name = document.getElementsByClassName("meal-name")[0];
+let meal_photo = document.querySelector("img");
+let meal_intr = document.getElementsByClassName("meal-instructions")[0];
 let search = document.getElementsByClassName("search-food")[0];
 const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=rice`;
-
+food();
+let meals;
 async function food() {
           let r = await fetch(api)
           let d = await r.json();
 
-          console.log(d.meals[0])
+          meals = d.meals[0]
+          console.log(meals.strMeal)
+          dispayFood()
 }
 
-food();
+function dispayFood() {
+          meal_name.textContent = meals.strMeal;
+          meal_photo.src = meals.strMealThumb;
+          meal_intr.textContent = meals.strInstructions;
+}
