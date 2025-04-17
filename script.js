48d9f88e21e6d4a367c0f7df5fb00e9884444cfc
odaysleft <vincentarasa254@gmail.com>
2025-04-17 13:26:34 +0300

    found a way round to make the search button work, but lots of code repetition

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
 
 
-// let showAllMeals = document.getElementsByClassName("show-meal")[0];
+let api = `https://www.themealdb.com/api/json/v1/1/search.php?s=`
+let showAllMeals = document.getElementsByClassName("show-meal")[0];
 // let meal_photo = document.querySelector("img");
 // let meal_intr = document.getElementsByClassName("meal-instructions")[0];
- let mealValue = document.getElementById("search-food");
-// let search = document.getElementById("searchBtn").addEventListener('click', 
-//      () => {
-//           food();
-//           console.log("Meal Value is: " + mealValue)
-//      }
-// )
-
-// let api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealValue || ""}`
-// let d;
-// food();
-// let meals;
-// async function food() {
-//           let r = await fetch(api)
-//           d = await r.json();
-
-//           console.log(d.meals)
-//           dispayFood()
-// }
-
-// function dispayFood() {
-
-//           console.log("There are : " + d.meals.length)
-//           for(let i = 0; i <= d.meals.length - 1; i++){
-//                 showAllMeals.innerHTML += `
-//                          <p class="meal-name">${d.meals[i].strMeal}</p>
-//                          <img src="${d.meals[i].strMealThumb}" class="meal-img">
-//                          <div class="meal-instructions">${d.meals[i].strInstructions}</div>
-//                `
-//           }
-// }
-
-
+let mealValue = document.getElementById("search-food");
+let mealValue_text;
 let search = document.getElementById("searchBtn").addEventListener('click', 
      () => {
+
+          if(!mealValue.value) return
           mealValue_text =  mealValue.value
-          console.log("Meal Value is: " + mealValue_text)
+          let api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealValue_text}`
+          showAllMeals.innerHTML = ""
           food();
+async function food() {
+          let r = await fetch(api)
+          d = await r.json();
+
+          dispayFood()
+}
+
+function dispayFood() {
+
+          for(let i = 0; i <= d.meals.length - 1; i++){
+                showAllMeals.innerHTML += `
+                         <p class="meal-name">${d.meals[i].strMeal}</p>
+                         <img src="${d.meals[i].strMealThumb}" class="meal-img">
+                         <div class="meal-instructions">${d.meals[i].strInstructions}</div>
+               `
+          }
+}
+
      }
 )
+
+let d;
+
+let meals;
+
+window.onload = () => {
+food();
+async function food() {
+          let r = await fetch(api)
+          d = await r.json();
+
+          dispayFood()
+}
+
+function dispayFood() {
+
+          for(let i = 0; i <= d.meals.length - 1; i++){
+                showAllMeals.innerHTML += `
+                         <p class="meal-name">${d.meals[i].strMeal}</p>
+                         <img src="${d.meals[i].strMealThumb}" class="meal-img">
+                         <div class="meal-instructions">${d.meals[i].strInstructions}</div>
+               `
+          }
+}
+
+}
 
