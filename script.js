f3e5038a9c9fd40cdcccb9365c7121f5e8aaf28a
odaysleft <vincentarasa254@gmail.com>
2025-04-17 12:30:20 +0300

    adding search functionality

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 
 let meal_name = document.getElementsByClassName("meal-name")[0];
+let showAllMeals = document.getElementsByClassName("show-meal")[0];
 let meal_photo = document.querySelector("img");
 let meal_intr = document.getElementsByClassName("meal-instructions")[0];
 let search = document.getElementsByClassName("search-food")[0];
-const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=rice`;
+const api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search|| ""}`;
+ let d;
 food();
 let meals;
 async function food() {
           let r = await fetch(api)
-          let d = await r.json();
+          d = await r.json();
 
           meals = d.meals[0]
-          console.log(meals.strMeal)
           dispayFood()
-}
+          console.log(d.meals.length)
+     }
 
 function dispayFood() {
-          meal_name.textContent = meals.strMeal;
-          meal_photo.src = meals.strMealThumb;
-          meal_intr.textContent = meals.strInstructions;
+
+          
+          for(let i = 0; i <= d.meals.length - 1; i++){
+               console.log(d.meals[i].strMeal);
+                showAllMeals.innerHTML += `
+                         <p class="meal-name">${d.meals[i].strMeal}</p>
+                         <img src="${d.meals[i].strMealThumb}" class="meal-img">
+                         <div class="meal-instructions">${d.meals[i].strInstructions}</div>
+               `
+          }
 }
