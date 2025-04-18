f3161969e5f7202fda058f01ab53e8b4dab8dad6
odaysleft <vincentarasa254@gmail.com>
2025-04-18 20:37:39 +0300

    created a function for a meal

--- scripts/meal-details.js
+++ scripts/meal-details.js
@@ -1,5 +1,19 @@
 
+let mealId = null;
 
-sessionStorage
+window.onload = () => {
+ mealId = sessionStorage.getItem('mealId')
+}
+let getMealById = `www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
 
-//www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
+let mealElementSection = document.querySelector('.meal-details');
+
+async function showMealDetails(){
+     let response = await fetch(getMealById);
+     let data = await response.json()
+
+     console.log(data)
+}
+
+showMealDetails()
