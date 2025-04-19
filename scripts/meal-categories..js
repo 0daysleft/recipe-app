f3b060238d7db7eca2833ef73df3e21855bdbc30
odaysleft <vincentarasa254@gmail.com>
2025-04-19 07:34:02 +0300

    display how to display each meal category(test)

--- scripts/meal-categories..js
+++ scripts/meal-categories..js
@@ -1,3 +1,25 @@
 
 
 
+let categoriesApi = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+
+
+async function fetchMealCategories(){
+
+     try{
+     let categoriesResponse = await fetch(categoriesApi);
+     let categoriesResponseData = await categoriesResponse.json();
+
+     console.log(categoriesResponseData.categories[0])
+     }
+     catch(err){
+          document.querySelector('.meal-categories').innerHTML = err;
+     }
+}
+
+fetchMealCategories()
+
+function displayMealCategories(){
+     document.querySelector('.meal-categories').innerHTML = ""
+}
+
