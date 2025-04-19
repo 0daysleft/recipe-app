07f20442e9915298628ec26e2daa6be7e0f5b8a9
odaysleft <vincentarasa254@gmail.com>
2025-04-19 09:02:56 +0300

    fetching meal category data function

---
+++ scripts/meal-category-type.js
@@ -0,0 +1,13 @@
+
+let mealCategoryTypeApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+let mealCategoryTypeApiResponseData;
+
+async function fetchMealCategoryTypeData(){
+     let mealCategoryTypeApiResponse = fetch(mealCategoryTypeApi)
+     mealCategoryTypeApiResponseData = (await mealCategoryTypeApiResponse).json()
+
+     
+}
+
+
+
