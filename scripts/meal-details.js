93c8302f1a0ec7931c81faf754f3e04b63e01e6a
odaysleft <vincentarasa254@gmail.com>
2025-04-19 06:18:15 +0300

    fixing ways how to display the elemets

--- scripts/meal-details.js
+++ scripts/meal-details.js
@@ -8,28 +8,25 @@ let getMealById = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId
 
 
 let mealElementSection = document.querySelector('.meal-details');
+let singleMealData;
 
 async function getMealDetails(){
      let response = await fetch(getMealById);
-     let singleMealData = await response.json()
+     singleMealData = await response.json()
 
     // console.log("Single: ", singleMealData.meals[0])
 
     const ingredient = Object.keys(singleMealData.meals[0]).filter(key => key.includes("strIngredient")).length;
     const measure = Object.keys(singleMealData.meals[0]).filter(key => key.includes("strMeasure")).length;
+ console.log(singleMealData.meals[0])
 
-    console.log("Meal: ", singleMealData.meals[0].strIngredient11)
-
-    console.log("Ingredient: ", ingredient,"Measure: ", measure)
-
-    
 function c(){
     let list = "";
     const meal = singleMealData.meals[0];
 
     for(let i = 1; i <= 20; i++) {
         let ingredient = meal[`strIngredient${i}`];
-        
+
         if(ingredient && ingredient.trim() !== "") {
             list += `<li>${ingredient}</li>`;
         }
@@ -47,16 +44,18 @@ ${c()}
  </ul>
 
 `
+
+showMealDetails();
 }
 
 getMealDetails()
 
 function showMealDetails(){
-     document.querySelector('meal-details').innerHTML
+     document.querySelector('.meal-details').innerHTML =
      `
           <a href="#"><h1>Dessert</h1></a>
           <h1>Pancakes</h1>
-          <img src="https://www.themealdb.com/images/media/meals/rwuyqx1511383174.jpg" alt="pancake-img"/>
+          <img src="${singleMealData.meals[0].strMealThumb}" alt="pancake-img"/>
 
           <div class="meal-ingredients">
                <h1>Meal Ingredients</h1>
