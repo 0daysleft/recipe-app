c01556be08de3b22826e59de2640775c4ef1d985
odaysleft <vincentarasa254@gmail.com>
2025-04-19 10:24:56 +0300

    FIXES A FEW THINGS, NAVIGATION, FUNCTIONS

--- scripts/meal-category-type.js
+++ scripts/meal-category-type.js
@@ -1,24 +1,42 @@
+/*
+
+IN THIS PAGE IS SHOULD FIX THE ISSUES OF ALL THE ELEMENTS FROM DISPLAYING HERE,
+
+ONLY THE CATEGORY TYPE SHOULD DISPLAY
+
+*/
+
+
+
 import { food, dispayFood } from "./script.js";
 
 let mealCategoryTypeApi = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${sessionStorage.getItem('categoryName')}`
+
+
+document.querySelector('.navigate').innerHTML = `<h1> <a href="../index.html">Home</a>/ <a href="../html-files/meal-categories.htm">Categories</a> / ${sessionStorage.getItem('categoryName')} </h1>`
+
+//THIS FUNCTION BELOW HERE SHOWS YOU HOW MANY ELEMNTS ARE IN THE CATEGORY PART
+
 // let mealCategoryTypeApiResponseData;
 
 // async function fetchMealCategoryTypeData(){
 //      let mealCategoryTypeApiResponse = await fetch(mealCategoryTypeApi)
 //      mealCategoryTypeApiResponseData = await mealCategoryTypeApiResponse.json()
 
-//      console.log( mealCategoryTypeApiResponseData.meals[0])
+//      console.log( mealCategoryTypeApiResponseData.meals)
 
      
 // }
+
+// fetchMealCategoryTypeData()
+
 //if(!document.getElementById("searchBtn")) return
 
 
 food(mealCategoryTypeApi)
 dispayFood()
 
-fetchMealCategoryTypeData()
 
-document.querySelector('.navigate').innerHTML = `<h1> <a href="../index.html">Home</a>/ <a href="../html-files/meal-categories.htm">Categories</a> ${categoryName} </h1>`
+
 
 
