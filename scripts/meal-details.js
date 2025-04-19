4488a5d4a20f586ce681b04874d3fd473eaddd24
odaysleft <vincentarasa254@gmail.com>
2025-04-19 06:25:56 +0300

    well fixed

--- scripts/meal-details.js
+++ scripts/meal-details.js
@@ -9,7 +9,8 @@ let getMealById = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId
 
 let mealElementSection = document.querySelector('.meal-details');
 let singleMealData;
-
+let displayIngredient;
+let displayMeasure;
 async function getMealDetails(){
      let response = await fetch(getMealById);
      singleMealData = await response.json()
@@ -20,11 +21,11 @@ async function getMealDetails(){
     const measure = Object.keys(singleMealData.meals[0]).filter(key => key.includes("strMeasure")).length;
  console.log(singleMealData.meals[0])
 
-function c(){
+  displayIngredient = () => {
     let list = "";
     const meal = singleMealData.meals[0];
 
-    for(let i = 1; i <= 20; i++) {
+    for(let i = 1; i <= ingredient; i++) {
         let ingredient = meal[`strIngredient${i}`];
 
         if(ingredient && ingredient.trim() !== "") {
@@ -32,18 +33,23 @@ function c(){
         }
     }
 
-    return list;
-}
+     return list;
+     }
 
+displayMeasure = () => {
+    let list = "";
+    const meal = singleMealData.meals[0];
 
-document.querySelector('#demo').innerHTML +=
+    for(let i = 1; i <= measure; i++) {
+        let measure = meal[`strMeasure${i}`];
 
-`
-  <ul>
-${c()}
- </ul>
+        if(measure && measure.trim() !== "") {
+            list += `<li>${measure}</li>`;
+        }
+    }
 
-`
+     return list;
+     }
 
 showMealDetails();
 }
@@ -60,31 +66,19 @@ function showMealDetails(){
           <div class="meal-ingredients">
                <h1>Meal Ingredients</h1>
                <ul>
-                         <li>Flour</li>
-                         <li>Eggs</li>
-                         <li>Milk</li>
-                         <li>Sunflower Oil</li>
-                         <li>Sugar</li>
-                         <li>Raspberries</li>
-                         <li>Blueberries</li>
+                         ${ displayIngredient()}
                </ul>
           </div>
 
           <div class="meal-measures">
                <h1>Meal Measures</h1>
                <ul>
-                    <li>100g</li>
-                    <li>2 large</li>
-                    <li>300ml</li>
-                    <li>1 tbls</li>
-                    <li>to serve</li>
-                    <li>to serve</li>
-                    <li>to serve</li>
+                    ${displayMeasure()}
                </ul>
           </div>
          <div class="meal-instructions">
           <h1>Cooking Instructions</h1>
-          <p>  ingredients </p>
+          <p>  ${singleMealData.meals[0].strInstructions} </p>
      </section>
      `
 }
