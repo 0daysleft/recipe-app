8b94c3f21354f123e77104c67cec184653069d1f
odaysleft <vincentarasa254@gmail.com>
2025-04-19 07:36:17 +0300

    functionality

--- scripts/meal-categories..js
+++ scripts/meal-categories..js
@@ -2,13 +2,13 @@
 
 
 let categoriesApi = 'https://www.themealdb.com/api/json/v1/1/categories.php'
-
+ let categoriesResponseData;
 
 async function fetchMealCategories(){
 
      try{
      let categoriesResponse = await fetch(categoriesApi);
-     let categoriesResponseData = await categoriesResponse.json();
+     categoriesResponseData = await categoriesResponse.json();
 
      console.log(categoriesResponseData.categories[0])
      }
@@ -20,6 +20,27 @@ async function fetchMealCategories(){
 fetchMealCategories()
 
 function displayMealCategories(){
-     document.querySelector('.meal-categories').innerHTML = ""
+
+     categoriesResponseData.categories[0]
+
+     document.querySelector('.meal-categories').innerHTML = `
+     
+          <div class="meal-category">
+                    <h3>Beef</h3>
+                    <img src="https://www.themealdb.com/images/category/beef.png" alt="beef" id="1">
+                    <p>Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times. Beef is a source of high-quality protein and essential nutrients.</p>
+          </div>
+
+
+     `
 }
+/*
+{
+    "idCategory": "1",
+    "strCategory": "Beef",
+    "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
+    "strCategoryDescription": "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]"
+}
+
+*/
 
