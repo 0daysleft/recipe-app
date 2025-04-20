437443fbd45ff3dff0e82c729bd92e6de630f5e0
odaysleft <vincentarasa254@gmail.com>
2025-04-20 19:09:17 +0300

    font awesome icon fixes

--- scripts/meal-categories..js
+++ scripts/meal-categories..js
@@ -4,7 +4,7 @@
 let categoriesApi = 'https://www.themealdb.com/api/json/v1/1/categories.php'
  let categoriesResponseData;
 
-document.querySelector('.navigate').innerHTML = `<h5> <a href="../index.html">Home</a> > Categories</h5>`
+document.querySelector('.navigate').innerHTML = `<h5> <a href="../index.html">Home</a> <i class="fa-solid fa-angle-right"></i> Categories</h5>`
 
 async function fetchMealCategories(){
 
