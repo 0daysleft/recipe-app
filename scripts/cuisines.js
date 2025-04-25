5a218477d8877e7eb32f76a59360e8dc4f060404
odaysleft <vincentarasa254@gmail.com>
2025-04-25 10:22:53 +0300

    function for getting cuisines

--- scripts/cuisines.js
+++ scripts/cuisines.js
@@ -1,3 +1,9 @@
 
 
+let cuisineApi = `https://https://www.themealdb.com/api/json/v1/1/list.php?a=list`
+
+const fetchCuisines = async () => {
+     let cuisineApiResponse = await fetch(cuisineApi);
+     let cuisineApiResponseData = await cuisineApiResponse.json()
+}
 
