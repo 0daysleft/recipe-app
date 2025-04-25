a2232599e1a4df16ffec860874587e968c7f709a
odaysleft <vincentarasa254@gmail.com>
2025-04-25 14:12:33 +0300

    done implementing the shuffling

--- scripts/cuisines.js
+++ scripts/cuisines.js
@@ -21,28 +21,28 @@ const fetchCuisines = async () => {
      // }
 
      function shuffledCuisines(array) {
-    for (let i = array.length - 1; i > 0; i--) { // Loop through the array backwards
-        let random = Math.floor(Math.random() * (i + 1)); // Generate a random index
-        [array[i], array[random]] = [array[random], array[i]]; // Swap elements
-    }
+          for (let i = array.length - 1; i > 0; i--) {
+               let random = Math.floor(Math.random() * (i + 1)); 
+               [array[i], array[random]] = [array[random], array[i]];
+     }
     return array;
 }
 
 
-     console.log(shuffledCuisines(arrayToShuffle))
-     console.log(cuisineApiResponseData.meals)
+     // console.log(shuffledCuisines(arrayToShuffle))
+     // console.log(cuisineApiResponseData.meals)
 
-     //  for(let i = 0; i <= 4; i++){
-     //           //console.log(data.meals[i].strArea);
-     //           //cuisinesArray.push(data.meals[i].strArea);
+      for(let i = 0; i <= 4; i++){
+               //console.log(data.meals[i].strArea);
+               //cuisinesArray.push(data.meals[i].strArea);
                
-     //           document.querySelector('.cuisines').innerHTML += 
-     //                          `
-     //                               <div class='cuisine'>
-     //                                    <p>${shuffledCuisines[i].strArea}</p>
-     //                               <div>
-     //                          `
-     //  }
+               document.querySelector('.cuisines').innerHTML += 
+                              `
+                                   <div class='cuisine'>
+                                        <p>${shuffledCuisines(arrayToShuffle)[i].strArea}</p>
+                                   <div>
+                              `
+      }
 
 }
 
