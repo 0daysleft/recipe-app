8a73c976fab847cb1b385b7a1eebc2b4914a5fef
odaysleft <vincentarasa254@gmail.com>
2025-04-25 10:31:53 +0300

    resolving naming conflicts

--- scripts/cuisines.js
+++ scripts/cuisines.js
@@ -1,23 +1,26 @@
 
 
-let cuisineApi = `https://https://www.themealdb.com/api/json/v1/1/list.php?a=list`
+let cuisineApi = `https://www.themealdb.com/api/json/v1/1/list.php?a=list`
 
 const fetchCuisines = async () => {
      let cuisineApiResponse = await fetch(cuisineApi);
      let cuisineApiResponseData = await cuisineApiResponse.json();
 
+     console.log(cuisineApiResponseData)
 
-      for(let i = 0; i <= cuisineApiResponseData.meals.length - 1; i++){
-               //console.log(data.meals[i].strArea);
-               //cuisinesArray.push(data.meals[i].strArea);
+     //  for(let i = 0; i <= cuisineApiResponseData.meals.length - 1; i++){
+     //           //console.log(data.meals[i].strArea);
+     //           //cuisinesArray.push(data.meals[i].strArea);
                
-               document.querySelector('.cuisines').innerHTML += 
-                              `
-                                   <div class='show-meal'>
-                                        <p>${cuisineApiResponseData.meals[i].strArea}</p>
-                                   <div>
-                              `
-      }
+     //           document.querySelector('.cuisines').innerHTML += 
+     //                          `
+     //                               <div class='show-meal'>
+     //                                    <p>${cuisineApiResponseData.meals[i].strArea}</p>
+     //                               <div>
+     //                          `
+     //  }
 
 }
+
+fetchCuisines()
 
