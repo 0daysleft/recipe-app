5fc62771b08f491369ac00c87a0841b7cfd8ed0b
0 Days Left <166432609+0daysleft@users.noreply.github.com>
2025-05-04 20:43:26 +0300

    Update meal-type.js by removing unused/ commented codes

--- scripts/meal-type.js
+++ scripts/meal-type.js
@@ -55,17 +55,9 @@ setTimeout(
           document.querySelector('footer').style.display = 'flex';
      }, 2000
 )
-
-     // document.querySelector('.meal-categories').innerHTML = `
-     
-     //      <div class="meal-category">
-     //                <h3>Beef</h3>
-     //                <img src="https://www.themealdb.com/images/category/beef.png" alt="beef" id="1">
-     //                <p>Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times. Beef is a source of high-quality protein and essential nutrients.</p>
-     //      </div> 
-     //`
-
 }
+
+                                        
 //Alert Message
 
 document.querySelector('nav .logo').addEventListener(
